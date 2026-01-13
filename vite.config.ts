105f4b01e56ca0159860b64b39769ddf8e153e05
miaow89 <limekatjq@gmail.com>
2026-01-13 22:03:40 +0900

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages 배포를 위해 베이스 경로를 상대 경로로 설정합니다.
+  base: './',
   define: {
     'process.env': process.env
   }
